96f12ccbdc0587991ace1dc32bb7be2ca9cf1301
huanghao <huanghao@yy.com>
2016-12-27 17:02:55 +0800

    Add agent provisioning script.

---
+++ configure-sigma-mesos-agent.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -x  # turn on trace
+set -e  # turn on exit immediately
+
+echo "==========================================="
+echo "  Install packages ..."
+echo "==========================================="
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
+apt-get update
+apt-get -y install mesos
+
+echo "==========================================="
+echo "  Install packages OK"
+echo "  Provisioning ..."
+echo "==========================================="
+
+stop zookeeper || true
+echo "manual" > /etc/init/zookeeper.override
+stop mesos-master || true
+echo "manual" > /etc/init/mesos-master.override
+echo "/data/mesos" > /etc/mesos-slave/work_dir
+echo $(grep ip_isp_list /home/dspeak/yyms/hostinfo.ini | sed 's/^ip_isp_list=//' | awk -F',' '{ for(i = 1; i <= NF; i++) { if ($i ~ /:CTL$/) print $i; } }' | sed 's/:CTL$//' | head -n 1) > /etc/mesos-slave/hostname
+cp /etc/mesos-slave/hostname /etc/mesos-slave/ip
+mkdir -p /etc/mesos-slave/attributes
+echo $(grep idc_id /home/dspeak/yyms/hostinfo.ini | sed 's/^idc_id=//' | head -n 1) > /etc/mesos-slave/attributes/idc_id
+echo "61.160.36.73:5050" > /etc/mesos/zk
+start mesos-slave || true
+
+echo "==========================================="
+echo "  Provisioning OK"
+echo "  Checking ..."
+echo "==========================================="
+
+status mesos-slave
+sleep 3
+cat /proc/$(pidof mesos-slave)/cmdline | tr '\0' '\n'
